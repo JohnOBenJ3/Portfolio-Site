ffa5e793d3020fba4059bbcb659d70b1b301629f
Orrin Johnson <orrinjohnson@Orrins-MacBook-Air.local>
2020-01-21 20:46:26 -0700

    testing a real deploy

--- orrin-johnson.com/src/App.svelte
+++ orrin-johnson.com/src/App.svelte
@@ -1,30 +1,25 @@
 <script>
-	export let name;
+  import Navbar from "./Components/Navbar.svelte";
+  import Banner from "./Components/Banner.svelte";
+  import Services from "./Components/Services.svelte";
+  import About from "./Components/About.svelte";
+  import Testimonials from "./Components/Testimonials.svelte";
+  import Social from "./Components/Social.svelte";
+  import Footer from "./Components/Footer.svelte";
+  import DATA from "./Data/data";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<!-- Navbar -->
+<Navbar navlists={DATA.NAVBAR_DATA} header={DATA.HEADER} />
+<!-- Banner -->
+<Banner bannerData={DATA.BANNER_DATA} } />
+<!-- Services -->
+<Services serviceData={DATA.SERVICE_DATA} />
+<!-- About -->
+<About aboutData={DATA.ABOUT_DATA} />
+<!-- Testimonials -->
+<Testimonials testimonialData={DATA.TESTIMONIAL_DATA} />
+<!-- Social Media -->
+<Social socialData={DATA.SOCIAL_DATA} />
+<!-- Footer -->
+<Footer footerData={DATA.FOOTER_DATA} header={DATA.HEADER} />
